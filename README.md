d5cac0f1cafe23560cf20aba7089d4e1b4d6a0e4
FuFa <alanmathisonturing@163.com>
2024-11-01 11:13:29 +0000

    更改cuda环境， 所有内容提交

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Deep learning library based on openmmlab
-
+# DeepLearning Projects based on openmmlab
+## 介绍
+这个库是深度学习项目汇总库, 是基于[OpenMMLab](https://github.com/open-mmlab)的, 会把工作中遇到的内容往里面添加
+## 依赖介绍
+1. torch
+2. openmmlab系列: 
+3. onnx系列: onnx, onnxruntime, onnxsim
+4. 模型剪枝: 
+## 安装
+### 开发环境
+```
+由于torch和mmcv的安装需要指定pip源, 所以可以先手动安装torch, mmengine, mmcv, 然后在安装requirements/develop.txt
+pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu118
+pip3 install mmengine==0.10.5
+pip3 install mmcv==2.2.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.4/index.html
+pip3 install -r requirements/develop.txt --index-url https://download.pytorch.org/whl/cu118
+```
---
+++ tools/traverse_registry.py
@@ -0,0 +1,18 @@
+from mmengine.registry import traverse_registry_tree, count_registered_modules
+from mmpretrain.utils import register_all_modules as register_all_mmpretrain_modules
+from mmseg.utils import register_all_modules as register_all_mmseg_modules
+from mmdet.utils import register_all_modules as register_all_mmdet_modules
+from mmyolo.utils import register_all_modules as register_all_mmyolo_modules
+from mmdlp.utils import register_all_modules as register_all_mmdlp_modules
+
+from mmengine.registry import MODELS
+
+if __name__ == "__main__":
+    register_all_mmpretrain_modules()
+    register_all_mmseg_modules()
+    register_all_mmdet_modules()
+    register_all_mmyolo_modules()
+    register_all_mmdlp_modules()
+    traverse_registry_tree(MODELS, verbose=True)
+    print("Number of registered modules: ", count_registered_modules())
+
