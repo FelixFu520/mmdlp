c54baf3e73b3ee3dd522d91a73343387293c3b91
root <felixfu>
2024-11-19 16:06:27 +0800

    export onnx

--- tools/open_explorer/yolov5/export_onnx.py
+++ tools/open_explorer/yolov5/export_onnx.py
@@ -1,3 +1,5 @@
+# 把这部分代码添加到train.py中, Build runner 后, 用于临时导出onnx
+import torch
 class Net(torch.nn.Module):
     def __init__(self, model):
         super(Net, self).__init__()
@@ -6,10 +8,10 @@ class Net(torch.nn.Module):
         x = self.model.extract_feat(x)
         return self.model.bbox_head.deploy_x_series(x)
 
-net = Net(self.model)
-torch.onnx.export(net, torch.rand((1,3,384,2048)), "/home/users/fa.fu/work/yolov5-1.onnx", opset_version=11)
+net = Net(runner.model)
+torch.onnx.export(net, torch.rand((1,3,384,2048)).cuda(), "/home/users/fa.fu/work/yolov5s.onnx", opset_version=11)
 
 import onnx
 import onnxsim
-model_opt, check_ok =  onnxsim.simplify("/home/users/fa.fu/work/yolov5-1.onnx")
-onnx.save(model_opt, "/home/users/fa.fu/work/yolov5-2.onnx")
+model_opt, check_ok =  onnxsim.simplify("/home/users/fa.fu/work/yolov5s.onnx")
+onnx.save(model_opt, "/home/users/fa.fu/work/yolov5s-simple.onnx")
