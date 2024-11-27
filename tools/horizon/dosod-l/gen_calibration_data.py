a937a983348d49962e788a6c51dcf42083200c27
fa.fu <alanmathisonturing@163.com>
2024-11-27 17:39:39 +0800

    gen data

--- tools/horizon/dosod-l/gen_calibration_data.py
+++ tools/horizon/dosod-l/gen_calibration_data.py
@@ -30,6 +30,23 @@ def gen_calibration_data_featuremap(data_dir: str, save_dir: str, height: int = 
         assert image.shape == (1, 3, height, width)
         image.tofile(os.path.join(save_dir, os.path.basename(image_path)[:-4] + ".npy"))
 
+def gen_calibration_data_nv12(data_dir: str, save_dir: str, height: int = 640, width: int = 2048):
+    all_images = []
+    for classes_name in os.listdir(data_dir):
+        images = [os.path.join(data_dir, classes_name, p) for p in os.listdir(os.path.join(data_dir,classes_name)) if p.endswith("jpg") or p.endswith("JPG")]
+        all_images.extend(images)
+    os.makedirs(save_dir, exist_ok=True)
+    for image_path in tqdm(all_images, desc="Generate Calibration Data"):
+        image = preprocess_custom(
+            image_path, 
+            height=height, 
+            width=width,
+        )
+        image = image[np.newaxis, ...]
+        image = image * 255 # 因为nv12输入需要整数的float, 所以这里乘以255; 减均值除方差的过程放到校准过程中, 放到onnx里面了
+        assert image.shape == (1, 3, height, width)
+        image.tofile(os.path.join(save_dir, os.path.basename(image_path)[:-4] + ".npy"))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate Calibration Data")
     parser.add_argument("--data_dir", type=str, 
@@ -49,3 +66,5 @@ if __name__ == "__main__":
 
     if args.mode == "featuremap":
         gen_calibration_data_featuremap(args.data_dir, args.save_dir, height=args.height, width=args.width)
+    elif args.mode == "nv12":
+        gen_calibration_data_nv12(args.data_dir, args.save_dir, height=args.height, width=args.width)
