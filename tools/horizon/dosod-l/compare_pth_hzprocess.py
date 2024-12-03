050eeb41cc34230ce7f61ebc6d239d21a5e02d56
fa.fu <alanmathisonturing@163.com>
2024-12-03 12:28:41 +0800

    compare

--- tools/horizon/dosod-l/compare_pth_hzprocess.py
+++ tools/horizon/dosod-l/compare_pth_hzprocess.py
@@ -9,6 +9,32 @@ from onnx import helper, checker, shape_inference
 import argparse
 from preprocess import preprocess_custom
 
+def yuv444_128_to_rgb(yuv_image):
+    """
+    将YUV444_128格式的图像数据转换为RGB格式
+    """
+    yuv_image = yuv_image.transpose(1, 2, 0)
+
+    # 提取Y、U、V分量（这里假设数据排列顺序是Y、U、V通道依次排列）
+    y = yuv_image[:, :, 0].astype(np.float32)
+    u = yuv_image[:, :, 1].astype(np.float32)
+    v = yuv_image[:, :, 2].astype(np.float32)
+
+    # r = (y + 128) +  0.005496863275766373 * v
+    # g = (y + 128) -  0.0013478432083502412 * u - 0.0028007845394313335 * v
+    # b = (y + 128) +  0.0069498042576014996 * u
+
+    r = y +  0.005496863275766373 * v
+    g = y -  0.0013478432083502412 * u - 0.0028007845394313335 * v
+    b = y +  0.0069498042576014996 * u
+
+
+    # 合并RGB通道
+    rgb_image = np.stack([r, g, b], axis=2)
+    rgb_image = rgb_image.clip(0, 255).astype(np.uint8)
+    rgb_image = rgb_image.transpose(2, 0, 1)
+    return rgb_image
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -40,36 +66,19 @@ if __name__ == "__main__":
     image_yuv444 = fun_t2.run_transform(image_nv12)
 
     # pth输入
-    fun_t3 = YUVTransformer("RGB")
-    image_rgb = fun_t3.run_transform(image_yuv444)
-
-    input_data = image_yuv444[np.newaxis, ...]
-
-
-    input_data = input_data[np.newaxis, ...]
-    input_data -= 128
-    input_data = input_data.astype(np.int8)
-    input_data = input_data.transpose(0, 2, 3, 1)
+    image_rgb = yuv444_128_to_rgb(image_yuv444).astype(np.float32)
+    image_rgb /= 255
 
     # 得到HzPreprocess输出
     sess = HB_ONNXRuntime(model_file=onnx_origin_path)
     input_names = [input.name for input in sess.get_inputs()]
     output_names = [output.name for output in sess.get_outputs()]
-    image = preprocess_custom(
-        image_path, 
-        height=height, 
-        width=width,
-    )
-    image = image * 255
-    image = np.expand_dims(image, axis=0)
-    fun_t = RGB2YUV444Transformer(data_format="CHW")
-    input_data = fun_t.run_transform(image[0])
-    input_data = input_data[np.newaxis, ...]
+    input_data = image_yuv444[np.newaxis, ...]
     feed_dict = {
-        input_names[0]: image,
+        input_names[0]: input_data,
     }
     outputs = sess.run(output_names, feed_dict)
     output = outputs[0][0]
 
-    diff = np.sum(np.abs(output - image_pth))
+    diff = np.sum(np.abs(output - image_rgb))
     print(diff)
